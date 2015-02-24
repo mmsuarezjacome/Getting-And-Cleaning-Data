b617f564ea3b37899f47c28a7e3f0fd354ee8066
mmsuarezjacome <mmsuarezjacome@myseneca.ca>
2015-02-24 12:27:14 -0500

    Create Grouping And Chaining With dplyr - Scripts.R

---
+++ Grouping And Chaining With dplyr - Scripts.R
@@ -0,0 +1,169 @@
+SUMMARIZE1
+
+# Compute four values, in the following order, from
+# the grouped data:
+#
+# 1. count = n()
+# 2. unique = n_distinct(ip_id)
+# 3. countries = n_distinct(country)
+# 4. avg_bytes = mean(size)
+#
+# A few thing to be careful of:
+#
+# 1. Separate arguments by commas
+# 2. Make sure you have a closing parenthesis
+# 3. Check your spelling!
+# 4. Store the result in pack_sum (for 'package summary')
+#
+# You should also take a look at ?n and ?n_distinct, so
+# that you really understand what is going on.
+
+pack_sum <- summarize(by_package,
+                      count = n(),
+                      unique = n_distinct(ip_id),
+                      countries = n_distinct(country),
+                      avg_bytes = mean(size))
+
+
+SUMMARIZE2
+
+# Don't change any of the code below. Just type submit()
+# when you think you understand it.
+
+# We've already done this part, but we're repeating it
+# here for clarity.
+
+by_package <- group_by(cran, package)
+pack_sum <- summarize(by_package,
+                      count = n(),
+                      unique = n_distinct(ip_id),
+                      countries = n_distinct(country),
+                      avg_bytes = mean(size))
+
+# Here's the new bit, but using the same approach we've
+# been using this whole time.
+
+top_countries <- filter(pack_sum, countries > 60)
+result1 <- arrange(top_countries, desc(countries), avg_bytes)
+
+# Print the results to the console.
+print(result1)
+
+
+SUMMARIZE3
+
+# Don't change any of the code below. Just type submit()
+# when you think you understand it. If you find it
+# confusing, you're absolutely right!
+
+result2 <-
+  arrange(
+    filter(
+      summarize(
+        group_by(cran,
+                 package
+        ),
+        count = n(),
+        unique = n_distinct(ip_id),
+        countries = n_distinct(country),
+        avg_bytes = mean(size)
+      ),
+      countries > 60
+    ),
+    desc(countries),
+    avg_bytes
+  )
+
+print(result2)
+
+
+SUMMARIZE4
+
+# Read the code below, but don't change anything. As
+# you read it, you can pronounce the %>% operator as
+# the word 'then'.
+#
+# Type submit() when you think you understand
+# everything here.
+
+result3 <-
+  cran %>%
+  group_by(package) %>%
+  summarize(count = n(),
+            unique = n_distinct(ip_id),
+            countries = n_distinct(country),
+            avg_bytes = mean(size)
+  ) %>%
+  filter(countries > 60) %>%
+  arrange(desc(countries), avg_bytes)
+
+# Print result to console
+print(result3)
+
+
+CHAIN1
+
+# select() the following columns from cran. Keep in mind
+# that when you're using the chaining operator, you don't
+# need to specify the name of the data tbl in your call to
+# select().
+#
+# 1. ip_id
+# 2. country
+# 3. package
+# 4. size
+#
+# The call to print() at the end of the chain is optional,
+# but necessary if you want your results printed to the
+# console. Note that since there are no additional arguments
+# to print(), you can leave off the parentheses after
+# the function name. This is a convenient feature of the %>%
+# operator.
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+	print()
+
+
+CHAIN2
+
+# Use mutate() to add a column called size_mb that contains
+# the size of each download in megabytes (i.e. size / 2^20).
+#
+# If you want your results printed to the console, add
+# print to the end of your chain.
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+  mutate(size_mb = size / 2^20) %>%
+  print()
+
+
+CHAIN3
+
+# Use filter() to select all rows for which size_mb is
+# less than or equal to (<=) 0.5.
+#
+# If you want your results printed to the console, add
+# print to the end of your chain.
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+  mutate(size_mb = size / 2^20) %>%
+  filter(size_mb <= 0.5) %>%
+  print()
+
+
+CHAIN4
+
+# arrange() the result by size_mb, in descending order.
+#
+# If you want your results printed to the console, add
+# print to the end of your chain.
+
+cran %>%
+  select(ip_id, country, package, size) %>%
+  mutate(size_mb = size / 2^20) %>%
+  filter(size_mb <= 0.5) %>%
+  arrange(desc(size_mb)) %>%
+  print()
